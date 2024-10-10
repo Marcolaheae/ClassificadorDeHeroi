baf87dc3e8490efa4f13db02ce9a070fbfd3e0cb
Marcolaheae <167900661+Marcolaheae@users.noreply.github.com>
2024-10-10 14:16:40 -0300

    projeto criado

---
+++ index.js
@@ -0,0 +1,35 @@
+let nome = 'teste'
+
+let pontos = 1
+
+  if (pontos <= 1000){
+  console.log('O herói de nome ' + nome + ' está no nível de Ferro')
+  }
+  
+  if (pontos >= 1001 && pontos <= 2000){
+  console.log('O herói de nome ' + nome + ' está no nível de Bronze')
+  }
+  
+  if (pontos > 2001 && pontos <= 5000){
+  console.log('O herói de nome ' + nome + ' está no nível de Prata')
+  }
+  
+  if (pontos > 5001 && pontos <= 7000){
+  console.log('O herói de nome ' + nome + ' está no nível de Ouro')
+  }
+  
+  if (pontos > 7001 && pontos <= 8000){
+  console.log('O herói de nome ' + nome + ' está no nível de Platina')
+  }
+  
+  if (pontos > 8001 && pontos <= 9000){
+  console.log('O herói de nome ' + nome + ' está no nível de Ascendente') 
+  }
+  
+  if (pontos > 9001 && pontos <= 10000){
+  console.log('O herói de nome ' + nome + ' está no nível de Imortal')
+  }
+  
+  if (pontos >= 10001){
+  console.log('O herói de nome ' + nome + ' está no nível de Radiante')
+  }
